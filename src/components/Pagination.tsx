f43ea47333c4c4979095e8c247be6de874a226ce
justin <justin@modusense.com>
2022-11-18 09:21:01 +1300

    improve on page button logic

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -24,7 +24,7 @@ export function Pagination(props: IProps) {
 
     function goToNextPage() {
         // setOffset(contentPerPage * (totalPage - 1));
-        setOffset((prev) => Math.min(prev + contentPerPage, totalPage));
+        setOffset((prev) => Math.min(prev + contentPerPage, contentPerPage * (totalPage - 1)));
     }
 
     function gotToPreviousPage() {
@@ -37,16 +37,49 @@ export function Pagination(props: IProps) {
             <div className="page-info">
                 Showing {from} to {to} of {total} items
             </div>
+
             {total > 0 && (
                 <div className="page-container">
                     <button onClick={gotToPreviousPage} disabled={currentPage === 1}>&lt;</button>
 
+                    {!pageArray.includes(1) && (
+                        <>
+                            <PaginationButton 
+                                key={1} 
+                                page={1} 
+                                offset={offset} 
+                                contentPerPage={contentPerPage} 
+                                onClick={() => {setOffset(0)}} 
+                            />
+                            <div>...</div>
+                        </>
+                    )}
+
                     {/* for each page in page array, set offset relative to page number */}
                     {pageArray.map((page) => {
                         return (
-                            <PaginationButton key={page} page={page} offset={offset} contentPerPage={contentPerPage} setOffset={setOffset} />
+                            <PaginationButton 
+                                key={page} 
+                                page={page} 
+                                offset={offset} 
+                                contentPerPage={contentPerPage} 
+                                onClick={() => {setOffset(contentPerPage * (page-1))}} 
+                            />
                         );
                     })}
+
+                    {!pageArray.includes(totalPage) && (
+                        <>
+                            <div>...</div>
+                            <PaginationButton 
+                                key={totalPage} 
+                                page={totalPage} 
+                                offset={offset} 
+                                contentPerPage={contentPerPage} 
+                                onClick={() => {setOffset(contentPerPage * (totalPage - 1))}} 
+                            />
+                        </>
+                    )}
 
                     <button onClick={goToNextPage} disabled={currentPage === totalPage}>&gt;</button>
                 </div>
--- src/components/PaginationButton.tsx
+++ src/components/PaginationButton.tsx
@@ -4,18 +4,20 @@ interface IProps {
     page: number;
     contentPerPage: number,
     offset: number,
-    setOffset: any,
+    onClick: any,
 }
 
 export function PaginationButton(props: IProps) {
     const currentPage = props.offset/props.contentPerPage + 1;
 
     return (
-        <button 
-            className={`page ${props.page === currentPage ? "isActive" : ""}`} 
-            onClick={() => props.setOffset(props.contentPerPage * (props.page-1))}
-        >
-            {props.page}
-        </button>
+        <>
+            <button 
+                className={`page ${props.page === currentPage ? "isActive" : ""}`} 
+                onClick={props.onClick}
+            >
+                {props.page}
+            </button>            
+        </>
     )
 }
